806b01c5c9667eac20cd18607b4c8569a01d4787
Arnau77 <Arnau7711@hotmail.com>
2022-04-06 14:15:45 +0200

    AI that only attacks implemented

---
+++ Adaptative AI/Assets/Scripts/AIs/OnlyAttackAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyAttackAI : AI
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void ChooseOption()
+    {
+        Player.Options optionChosen = Player.Options.SPECIAL_ATTACK;
+        if (!CheckIfOptionIsValid(optionChosen))
+        {
+            optionChosen=Player.Options.ATTACK;
+        }
+
+        player.DecideOption(optionChosen);
+    }
+}
---
+++ Adaptative AI/Assets/Scripts/AIs/SimpleAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAI : AI
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void ChooseOption()
+    {
+        bool optionValid;
+        Player.Options optionChosen;
+        do
+        {
+            optionChosen = (Player.Options)Random.Range(1, 8);
+            optionValid = CheckIfOptionIsValid(optionChosen);
+        } while (!optionValid);
+        player.DecideOption(optionChosen);
+    }
+}
